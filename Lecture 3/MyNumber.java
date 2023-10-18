24c5729bba5a5b3719f3659984b516d38e18ef3e
Edis <128993970+ShockWawe1000@users.noreply.github.com>
2023-10-18 18:49:53 +0200

    Update MyNumber.java

--- Lecture 3/MyNumber.java
+++ Lecture 3/MyNumber.java
@@ -9,10 +9,28 @@ public class MyNumber {
 		num=0;
 		num2=100;
 	}
+	public MyNumber(int num1,int num2) {
+		this.num=num1;
+		this.num2=num2;
+	}
 	
-	int getNum() {
+	int getNum1() {
 		return num;
 		
+	}
+	
+	int getNum2() {
+		return num2;
+	}
+	
+	void description()
+	{
+		System.out.println(this.getNum1());
+		System.out.println(this.getNum2());
+	}
+	
+	float average() {
+		return (this.num+this.num2)/2;
 	}
 	
 	void setNum(int i) {
