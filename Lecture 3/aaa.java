77f92bda74505b501469f6a11e0e20b5c6bb662e
Edis <128993970+ShockWawe1000@users.noreply.github.com>
2023-10-18 18:32:29 +0200

    Create aaa.java

---
+++ Lecture 3/aaa.java
@@ -0,0 +1,14 @@
+package aaa;
+
+
+
+public class aaa {
+public static void main(String[] args)
+{
+	MyNumber a = new MyNumber();
+	System.out.println(a.getNum());
+	a.setNum(10);
+	System.out.println(a.getNum());
+	System.out.println(a.num2);
+}
+}
